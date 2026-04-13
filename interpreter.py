443f8a06a0d10b313e3ef4a4894ce54b7db6cd9e
Pedro Marques Correa Domingues <pedromarquescd@gmail.com>
2026-04-13 10:56:56 -0300

    feat: adiciona avaliação de expressões no interpretador

--- interpreter.py
+++ interpreter.py
@@ -1,13 +1,34 @@
 variables = {}
 
 def interpret(tokens):
-    # var x = 10
+    # Caso: var x = ...
     if tokens[0][0] == "VAR":
         name = tokens[1][1]
-        value = int(tokens[3][1])
+
+        # Caso simples: var x = 10
+        if len(tokens) == 4:
+            value = int(tokens[3][1])
+
+        # Caso com expressão: var x = 5 + 3
+        else:
+            num1 = int(tokens[3][1])
+            op = tokens[4][0]
+            num2 = int(tokens[5][1])
+
+            if op == "PLUS":
+                value = num1 + num2
+            elif op == "MINUS":
+                value = num1 - num2
+            elif op == "MULT":
+                value = num1 * num2
+            elif op == "DIV":
+                value = num1 / num2
+            else:
+                raise Exception("Operador inválido")
+
         variables[name] = value
 
-    # print(x)
+    # Caso: print(x)
     elif tokens[0][0] == "PRINT":
         name = tokens[2][1]
 
