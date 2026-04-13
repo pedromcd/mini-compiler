ea7c46b56b1da866dce2a130baf369b9da08ddd3
Pedro Marques Correa Domingues <pedromarquescd@gmail.com>
2026-04-13 11:07:44 -0300

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -1,30 +1,26 @@
 variables = {}
 
 def interpret(tokens):
+    print("DEBUG INTERPRETADOR NOVO")
+def interpret(tokens):
     # Caso: var x = ...
     if tokens[0][0] == "VAR":
         name = tokens[1][1]
 
-        # Caso simples: var x = 10
-        if len(tokens) == 4:
-            value = int(tokens[3][1])
+        # pega toda a expressão depois do '='
+        expression_tokens = tokens[3:]
 
-        # Caso com expressão: var x = 5 + 3
-        else:
-            num1 = int(tokens[3][1])
-            op = tokens[4][0]
-            num2 = int(tokens[5][1])
+        # monta a expressão como string
+        expression = ""
+        for token in expression_tokens:
+            expression += token[1]
 
-            if op == "PLUS":
-                value = num1 + num2
-            elif op == "MINUS":
-                value = num1 - num2
-            elif op == "MULT":
-                value = num1 * num2
-            elif op == "DIV":
-                value = num1 / num2
-            else:
-                raise Exception("Operador inválido")
+        print("Expressão:", expression)  # debug (pode remover depois)
+
+        try:
+            value = eval(expression)
+        except:
+            raise Exception("Erro ao avaliar expressão")
 
         variables[name] = value
 
